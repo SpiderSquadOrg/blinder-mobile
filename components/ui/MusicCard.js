4177e9c36517c4ee2d410eaf5ea3cc8860fc1629
Batuhan Dikmen <rbdikmen@gmail.com>
2023-12-03 18:06:01 +0300

    Fix header display in Home and Likes screens

---
+++ components/ui/MusicCard.js
@@ -0,0 +1,48 @@
+import { View, StyleSheet, Dimensions, Image, Text } from "react-native";
+
+const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
+
+function MusicCard({ musicList }) {
+  return (
+    <View>
+      {musicList.map((music, index) => (
+        <View key={index} style={styles.container}>
+          <Image source={{ uri: music.imageUrl }} style={styles.musicImage} />
+          <View style={styles.textContainer}>
+            <Text style={styles.title}>{music.title}</Text>
+            <Text style={styles.artist}>{music.artist}</Text>
+          </View>
+        </View>
+      ))}
+    </View>
+  );
+}
+
+export default MusicCard;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    padding: 16,
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  musicImage: {
+    width: 60,
+    height: 60,
+    marginRight: screenWidth * 0.05,
+    borderRadius: 12,
+  },
+  textContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  artist: {
+    fontSize: 14,
+    color: "#666",
+  },
+});
--- screens/Home/ChatScreen.js
+++ screens/Home/ChatScreen.js
@@ -1,14 +1,125 @@
 import React, { useEffect, useState } from "react";
-import { Dimensions, StyleSheet, View } from "react-native";
-import PrimaryButton from "../../components/Button/PrimaryButton";
-import Title from "../../components/ui/Title";
+import {
+  Dimensions,
+  Keyboard,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Colors from "../../constansts/Colors";
+import { Appbar, Avatar } from "react-native-paper";
+import ChatDialog from "../../containers/Chat/ChatDialog";
+import BetterInputField from "../../components/ui/BetterInputField";
+import { Ionicons } from "@expo/vector-icons";
 
 const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
 
 function ChatScreen({ navigation, route }) {
+  const [message, setMessage] = useState("");
+  const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
+
+  const [userInChat, setUserInChat] = useState({
+    name: "Nine Tailed Fox",
+  });
+
+  const [messages, setMessages] = useState([
+    { text: "Lorem ipsum dolor sit amet", isMe: true, timestamp: new Date() },
+    {
+      text: "Lorem ipsum dolor sit amet too!",
+      isMe: false,
+      timestamp: new Date(),
+    },
+  ]);
+
+  const handleSendButton = () => {
+    const trimmedMessage = message.replace(/^\n+|\n+$/g, "").trim();
+
+    if (!trimmedMessage) {
+      return;
+    }
+
+    const newMessage = {
+      text: trimmedMessage,
+      isMe: true,
+      timestamp: new Date(),
+    };
+    setMessages((prevMessages) => [...prevMessages, newMessage]);
+    setMessage("");
+  };
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      "keyboardDidShow",
+      () => {
+        setIsKeyboardOpen(true);
+      }
+    );
+
+    const keyboardDidHideListener = Keyboard.addListener(
+      "keyboardDidHide",
+      () => {
+        setIsKeyboardOpen(false);
+      }
+    );
+
+    return () => {
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Title>Chats</Title>
+    <View
+      style={[
+        styles.container,
+        isKeyboardOpen && Platform.OS === "android"
+          ? { marginBottom: 0 }
+          : undefined,
+      ]}
+    >
+      <View style={styles.appbar}>
+        <Appbar.BackAction
+          color={Colors.primary600}
+          size={32}
+          onPress={() => navigation.navigate("MatchScreen")}
+        />
+        <Text style={styles.username}>{userInChat.name}</Text>
+        <Avatar.Image size={48} source={{}} style={styles.avatar} />
+      </View>
+      <View style={styles.chatDialog}>
+        <ChatDialog messages={messages} />
+      </View>
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+        style={[
+          styles.keyboardAvoidingContainer,
+          message.includes("\n") ? { marginTop: 20 } : undefined,
+        ]}
+      >
+        <View style={styles.inputContainer}>
+          <BetterInputField
+            placeholder="Type a message"
+            text={message}
+            setText={setMessage}
+            style={{ flex: 1 }}
+          />
+          {message.length > 0 ? (
+            <TouchableOpacity
+              onPress={handleSendButton}
+              style={styles.sendButton}
+            >
+              <View style={styles.roundButton}>
+                <Ionicons name="ios-send" size={17} color="white" />
+              </View>
+            </TouchableOpacity>
+          ) : (
+            ""
+          )}
+        </View>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -17,8 +128,54 @@ export default ChatScreen;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#fff",
+    flex: 1,
     alignItems: "center",
-    height: "100%",
+    justifyContent: "flex-end",
+    marginBottom: 65, // Adjust this value based on your design
+  },
+  appbar: {
+    marginTop: screenHeight * 0.04,
+    marginBottom: screenHeight * 0.02,
+    width: "100%",
+    height: 50,
+    alignItems: "center",
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+  avatar: {
+    marginRight: 10,
+  },
+  username: {
+    textAlign: "center",
+    fontSize: 20,
+    fontStyle: "normal",
+    fontWeight: "600",
+    lineHeight: 30,
+  },
+  keyboardAvoidingContainer: {
+    width: "100%",
+  },
+  chatDialog: {
+    flex: 1,
+    width: "100%",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    marginBottom: 10,
+  },
+  sendButton: {
+    marginRight: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  roundButton: {
+    width: 34,
+    height: 34,
+    backgroundColor: "#209de0", // İstediğiniz renge göre değiştirin
+    borderRadius: 17, // Yarıçapı genişliğinin yarısı kadar yapın
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
