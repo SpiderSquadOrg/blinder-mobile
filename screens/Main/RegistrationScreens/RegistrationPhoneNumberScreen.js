03a4c4754468e1657884387f94613e5a8eca1a15
irema <iremakanoglu@eskisehir.edu.tr>
2023-12-14 20:12:58 +0300

    Phone number screen is updated

--- screens/Main/RegistrationScreens/RegistrationPhoneNumberScreen.js
+++ screens/Main/RegistrationScreens/RegistrationPhoneNumberScreen.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect, useRef } from "react";
-import { View, StyleSheet, Alert, Dimensions } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Dimensions, ScrollView } from "react-native";
 import PhoneInput from "react-native-phone-input";
-import CountryPicker from "react-native-country-picker-modal";
 
 import TextButton from "../../../components/Button/TextButton";
 import RegistrationQueryText from "../../../components/ui/RegistrationQueryText";
@@ -9,53 +8,35 @@ import RegistrationQueryText from "../../../components/ui/RegistrationQueryText"
 const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
 
 function RegistrationPhoneNumberScreen({ navigation }) {
-  const phoneInputRef = useRef(null);
-  const countryPickerRef = useRef(null);
-
   const [phoneNumber, setPhoneNumber] = useState("");
-  const [countryCode, setCountryCode] = useState("");
   const [selectedCountry, setSelectedCountry] = useState(null);
 
   useEffect(() => {}, [selectedCountry]);
 
-  const selectCountry = (value) => {
-    phoneInputRef.current.selectCountry(value.cca2);
-    setSelectedCountry(value);
-  };
-
   const nextPageHandler = () => {
-    Alert.alert(
-      "Form Submitted",
-      `Phone Number: ${phoneNumber}\nCountry Code: ${countryCode}`
-    );
     navigation.navigate("RegistrationPartnerGenderScreen");
   };
 
   return (
-    <View>
+    <ScrollView>
       <RegistrationQueryText>TELEFON NUMARANIZ NEDİR ?</RegistrationQueryText>
       <View style={styles.container}>
         <PhoneInput
           value={phoneNumber}
+          initialCountry={"tr"}
           onChangePhoneNumber={(number) => setPhoneNumber(number)}
           style={styles.phoneInput}
+          textProps={{
+            placeholder: "Enter a phone number...",
+          }}
         />
-
-        <CountryPicker
-          //ref={countryPickerRef}
-          onChange={(value) => selectCountry(value)}
-          translation="eng"
-        >
-          <View></View>
-        </CountryPicker>
-
         <View style={styles.buttonContainer}>
           <TextButton onPress={nextPageHandler} style={styles.textButton}>
             İleri
           </TextButton>
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
